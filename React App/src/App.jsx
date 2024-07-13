c40e757c306f7934ae996d599e3f2623b198ffe5
Partha Pratim Deka <parthapratimdeka129@gmail.com>
2024-07-14 00:56:42 +0530

    UPDATE: react site

--- React App/src/App.jsx
+++ React App/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { DotLottieReact } from '@lottiefiles/dotlottie-react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="container">
+        <DotLottieReact src="/done.lottie" autoplay />
+        <h2>Welcome to a Random Site</h2>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          This page shows that you've successfully deployed this React Site
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
